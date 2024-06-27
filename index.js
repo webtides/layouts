07ae771b10c4d167ed1445830f1c130698b385ff
Eddy Löwen <mail@eddyloewen.com>
2024-06-27 19:55:51 +0200

    chore: adds some more defualts from tailwind config

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ export default (options = {}) => {
             md: '768px',
             lg: '1024px',
             xl: '1280px',
+            '2xl': '1536px',
         },
         gap: {
             0: '0',
